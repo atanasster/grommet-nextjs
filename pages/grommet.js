4fd9baf8ca1bffb331a83c88cee7ffab4e736a04
atanasster <atanasster@gmail.com>
2018-02-23 00:46:06 -0500

    added grommet page

---
+++ pages/grommet.js
@@ -0,0 +1,10 @@
+import { Grommet } from 'grommet';
+import doc from 'grommet/components/Grommet/doc';
+
+import Doc from '../components/Doc';
+
+const desc = doc(Grommet).toJSON();
+
+export default () => (
+  <Doc name='Grommet' desc={desc} />
+);
