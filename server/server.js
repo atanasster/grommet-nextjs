b68e617c5102f76fe2f2453f2e49c23910c052b3
atanasster <atanasster@gmail.com>
2018-03-06 11:59:42 +0200

    robots.txt

--- server/server.js
+++ server/server.js
@@ -1,21 +1,17 @@
 const express = require('express');
 const next = require('next');
+const staticFiles = require('./static');
 
 const port = parseInt(process.env.PORT, 10) || 3000;
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
+
 app.prepare()
   .then(() => {
     const server = express();
-
-    server.get('/a', (req, res) => app.render(req, res, '/b', req.query));
-
-    server.get('/b', (req, res) => app.render(req, res, '/a', req.query));
-
-    server.get('/posts/:id', (req, res) => app.render(req, res, '/posts', { id: req.params.id }));
-
+    server.use('/', staticFiles())
     server.get('*', (req, res) => handle(req, res));
 
     server.listen(port, (err) => {
