aab0a93bee52e8e5e902baf8da02db6956fb6bd8
atanasster <atanasster@gmail.com>
2018-10-06 11:02:07 +0300

    weight prop type

--- examples/Text/weight.mjs
+++ examples/Text/weight.mjs
@@ -1,7 +1,7 @@
 export const weight = `const Demo = () => (
   <Box direction='row' wrap={true}>
-    {['normal', 'bold', '200', '300' , '600', '700'].map(weight => (
-      <Box key={weight} margin='xsmall'>
+    {['normal', 'bold', 200, 300 , 600, 700].map(weight => (
+      <Box key={weight.toString()} margin='xsmall'>
         <Text weight={weight}>
           {weight}
         </Text>
