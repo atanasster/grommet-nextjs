1cfdb2d44255b5efc1390b7fd3c11feb13780a88
atanasster <atanasster@gmail.com>
2018-10-12 03:29:33 +0300

    meter example size

--- examples/Meter/_starter.mjs
+++ examples/Meter/_starter.mjs
@@ -1,13 +1,15 @@
 // eslint-disable-next-line no-underscore-dangle
 export const _starter = `const Demo = () => (
-  <Meter
-    aria-label='Meter example'
-    type='circle'
-    size='small'
-    thickness='large'
-    round={true}
-    values={[{ value: 60, label: 'sixty' }]}
-  />
+  <Box align='center'>
+    <Meter
+      aria-label='Meter example'
+      type='circle'
+      size='small'
+      thickness='large'
+      round={true}
+      values={[{ value: 60, label: 'sixty' }]}
+    />
+  </Box>  
 );
 
 render(<Demo />);  
