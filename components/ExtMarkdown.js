a6bc5de334170476d4a70a7baf380864a041ec29
atanasster <atanasster@gmail.com>
2019-02-08 07:25:03 -0500

    preserve params(theme) in markdown pages anchors

--- components/ExtMarkdown.js
+++ components/ExtMarkdown.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled, { withTheme } from 'styled-components';
 import { Image, Markdown, Box, Paragraph } from 'grommet';
-
+import RoutedAnchor from './RoutedAnchor';
 
 const LargeParagraph = styled(Paragraph)`
   max-width: 632px;
@@ -20,6 +20,7 @@ export default withTheme(({ theme, ...rest }) => (
     components={{
       p: { component: LargeParagraph },
       img: { component: MarkdownImage },
+      a: { component: RoutedAnchor },
       code: { component: Code, props: { background: theme.dark ? 'dark-1' : 'light-2' } },
     }}
     {...rest}
