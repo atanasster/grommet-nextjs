b3b95c33a3ca632289c59b8305e70e9c100f7eed
atanasster <atanasster@gmail.com>
2019-02-08 22:30:31 -0500

    update gatsby page

---
+++ pages/gatsby.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Article from '../components/Article';
+
+export default () => (
+  <Article
+    title='gatsby tree shaking'
+    owner='atanasster'
+    repo='grommet-controls-starter'
+    path='gatsby/README.md'
+  />
+);
