bce491dc4ae860496f27f65d44afaff2561519f1
atanasster <atanasster@gmail.com>
2019-02-08 19:05:09 -0500

    update to tree-shaking page

--- pages/tree-shaking.js
+++ pages/tree-shaking.js
@@ -4,9 +4,8 @@ import Article from '../components/Article';
 export default () => (
   <Article
     title='Tree shaking'
-    location='file'
     owner='atanasster'
-    repo='grommet-nextjs'
-    path='tree-shaking'
+    repo='grommet-controls-starter'
+    path='README.md'
   />
 );
