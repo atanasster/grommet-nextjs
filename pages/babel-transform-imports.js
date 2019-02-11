ab48f9f0baa9c7df54252ea198bb6e7c5a25b7d5
atanasster <atanasster@gmail.com>
2019-02-10 19:57:21 -0500

    transform-imports page

---
+++ pages/babel-transform-imports.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Article from '../components/Article';
+
+export default () => (
+  <Article
+    title='webpack < 4'
+    owner='atanasster'
+    repo='grommet-controls-starter'
+    path='babel-transform-imports/README.md'
+  />
+);
