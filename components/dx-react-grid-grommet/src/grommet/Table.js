079109c44332530521c774504e9538c244c86251
atanasster <atanasster@gmail.com>
2019-03-10 17:45:20 -0400

    transfer inner ref

--- components/dx-react-grid-grommet/src/grommet/Table.js
+++ components/dx-react-grid-grommet/src/grommet/Table.js
@@ -1,23 +1,49 @@
 import React from 'react';
-import styled, { withTheme, ThemeContext } from 'styled-components';
-import { deepMerge } from 'grommet/utils';
+import styled, { ThemeContext } from 'styled-components';
+import { deepMerge, normalizeColor } from 'grommet/utils';
 import { genericStyles } from 'grommet/utils/styles';
 import { defaultTheme } from './dx-theme';
 
 const StyledTable = styled.table`
+  width: 100%;
   border-spacing: 0;
   border-collapse: collapse;
-  width: inherit;
+  ${props => props.sticky && `
+    position: sticky;
+    z-index: 500;
+    overflow: visible;
+    background: ${normalizeColor('background', props.theme)};
+    fallbacks: {
+      position: -webkit-sticky;
+    }
+  `}
+  ${props => (props.use === 'head') && `
+    top: 0;
+  `}
+  ${props => (props.use === 'foot') && `
+    bottom: 0;
+  `} 
+
   ${genericStyles}
   ${props => props.theme.dxgrid && props.theme.dxgrid.extend};
 `;
 
+class TableClass extends React.Component {
+  render() {
+    return (
+      <ThemeContext.Consumer>
+        {(theme) => {
+          const extTheme = deepMerge(defaultTheme, theme);
+          return (
+            <ThemeContext.Provider value={extTheme}>
+              <StyledTable theme={extTheme} {...this.props} />
+            </ThemeContext.Provider>
+          );
+        }}
+      </ThemeContext.Consumer>
+    );
+  }
+}
 
-export const Table = withTheme(({ theme, ...rest }) => {
-  const extTheme = deepMerge(defaultTheme, theme);
-  return (
-    <ThemeContext.Provider value={extTheme}>
-      <StyledTable theme={extTheme} {...rest} />
-    </ThemeContext.Provider>
-  );
-});
+
+export const Table = TableClass;
--- components/dx-react-grid-grommet/src/templates/table.js
+++ components/dx-react-grid-grommet/src/templates/table.js
@@ -1,47 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import styled, { withTheme } from 'styled-components';
-import RootRef from '@material-ui/core/RootRef';
-import { RefType } from '@devexpress/dx-react-core';
-import { normalizeColor } from 'grommet/utils';
 import { Table as GrommetTable } from '../grommet/Table';
 
 
-const StyledTable = styled(GrommetTable)`
-  width: 100%;
-  ${props => props.sticky && `
-    position: sticky;
-    z-index: 500;
-    overflow: visible;
-    background: ${normalizeColor('background', props.theme)};
-    fallbacks: {
-      position: -webkit-sticky;
-    }
-  `}
-  ${props => (props.use === 'head') && `
-    top: 0;
-  `}
-  ${props => (props.use === 'foot') && `
-    bottom: 0;
-  `} 
-`;
-export const Table = withTheme(({
+export const Table = ({
   tableRef, use, theme, ...rest
 }) => (
-  <RootRef rootRef={tableRef}>
-    <StyledTable
-      sticky={!!use}
-      use={use}
-      theme={theme}
-      {...rest}
-    />
-  </RootRef>
-));
+  <GrommetTable
+    ref={tableRef}
+    sticky={!!use}
+    use={use}
+    {...rest}
+  />
+);
 
 Table.propTypes = {
   use: PropTypes.oneOf(['head', 'foot']),
   children: PropTypes.node.isRequired,
-  tableRef: RefType.isRequired,
 };
 
 Table.defaultProps = {
