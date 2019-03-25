e05b12cff9623481f3045e8a6ff2ccf1dfaf62b0
atanasster <atanasster@gmail.com>
2019-03-25 00:38:17 -0400

    detail-row examples

---
+++ docs/templates/dx-grid/detail-row/controlled.js
@@ -0,0 +1,62 @@
+/*
+import React from 'react';
+import { RowDetailState } from '@devexpress/dx-react-grid';
+import {
+  Grid,
+  Table,
+  TableHeaderRow,
+  TableRowDetail,
+} from 'dx-react-grid-grommet';
+import { Box, Text } from 'grommet';
+import { generateRows } from '../../../data/dx-grid-data/generator';
+*/
+
+const RowDetail = ({ row }) => (
+  <Box pad='medium' background='brand'>
+    <Text>
+      {`Details for ${row.name} from ${row.city}`}
+    </Text>
+  </Box>
+);
+
+class Demo extends React.PureComponent {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      columns: [
+        { name: 'name', title: 'Name' },
+        { name: 'sex', title: 'Sex' },
+        { name: 'city', title: 'City' },
+        { name: 'car', title: 'Car' },
+      ],
+      rows: generateRows({ length: 8 }),
+      expandedRowIds: [2, 5],
+    };
+
+    this.changeExpandedDetails = expandedRowIds => this.setState({ expandedRowIds });
+  }
+
+  render() {
+    const { rows, columns, expandedRowIds } = this.state;
+
+    return (
+      <Grid
+        rows={rows}
+        columns={columns}
+      >
+        <RowDetailState
+          expandedRowIds={expandedRowIds}
+          onExpandedRowIdsChange={this.changeExpandedDetails}
+        />
+        <Table />
+        <TableHeaderRow />
+        <TableRowDetail
+          contentComponent={RowDetail}
+        />
+      </Grid>
+    );
+  }
+}
+
+render(<Demo />);
